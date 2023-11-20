8c879815cf90354d1338820374292b95f8c9523b
이민제 <Ol.MinJe@gmail.com>
2023-11-20 16:57:46 +0900

    TodoList 일정 수정 구현 성공

--- src/App.js
+++ src/App.js
@@ -17,11 +17,16 @@ function App() {
     nextId.current += 1;
   });
   const onRemove = useCallback((id) => setTodos(todos.filter((todo) => todo.id !== id)), [todos]);
+  const onToggle = useCallback(
+    (id) =>
+      setTodos(todos.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo))),
+    [todos],
+  );
 
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} onRemove={onRemove} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 }
