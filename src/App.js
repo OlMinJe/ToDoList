24a71d2ebc5346f04517042b99c8d91a7a9c3c45
이민제 <Ol.MinJe@gmail.com>
2023-11-20 16:29:22 +0900

    TodoList/TodoListItem .js .scss 정적 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '할 일 추가2',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '할 일 추가1',
+      checked: true,
+    },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
