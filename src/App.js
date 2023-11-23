c0a52e1fb5b42c5a8f8f8d5b0172dc48bd88b6f4
이민제 <Ol.MinJe@gmail.com>
2023-11-23 17:17:22 +0900

    onInsert, onRemove, onToggle 함수를 함수형 업데이트 방식으로 변경
    TodoListItem에 React.memo를 사용하면 Todos 배열이 업데이트 될때마다 함수가 새로 만들어지는 현상이 일어남을 방지함.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,26 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
+/* 231123.ver
+ * 성능 테스트를 위해 2000개의 할 일을 넣어봄
+ */
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2000; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
 function App() {
-  const [todos, setTodos] = useState([]);
+  /* 231123.ver
+   * 파라미터 대신 함수형 업데이트로 변경
+   */
+  const [todos, setTodos] = useState(createBulkTodos);
   const nextId = useRef(1);
   const onInsert = useCallback((text) => {
     const todo = {
@@ -13,14 +31,32 @@ function App() {
       text,
       checked: false,
     };
-    setTodos(todos.concat(todo));
+    /* 231123.ver
+     * setTodos(todos.concat(todo));
+     * 위 코드 대신 함수형 업데이트로 바꿔준다.
+     */
+    setTodos((todos) => todos.concat(todo));
     nextId.current += 1;
   });
-  const onRemove = useCallback((id) => setTodos(todos.filter((todo) => todo.id !== id)), [todos]);
+
+  const onRemove = useCallback((id) => {
+    /* 231123.ver
+     * setTodos(todos.filter((todo) => todo.id !== id)), [todos];
+     * 위 코드 대신 함수형 업데이트로 바꿔준다.
+     */
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
+
   const onToggle = useCallback(
     (id) =>
-      setTodos(todos.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo))),
-    [todos],
+      /* 231123.ver
+       * setTodos(todos.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo))), [todos],
+       * 위 코드 대신 함수형 업데이트로 바꿔준다.
+       */
+      setTodos((todos) =>
+        todos.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo)),
+      ),
+    [],
   );
 
   return (
